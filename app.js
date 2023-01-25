5b27105a5579c2c9ee680d00385f2452ba56c477
Yuriy-Bolotnyy <yuriy.bolotnyy.cohort225@gmail.com>
2023-01-24 18:04:59 -0800

    done and works

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const SECRET_KEY = 'secretkey'
+
 const express = require('express');
 const jwt = require('jsonwebtoken');
 
@@ -13,10 +15,26 @@ app.listen(5000, () => {
     console.log('Server started on port 5000');
 });
 
-app.post('/api/posts', (req, res)=> {
-    res.json({
-        message: 'Post created ...'
-    });
+// Basic version - no authorization check
+// app.post('/api/posts', (req, res)=> {
+//     res.json({
+//         message: 'Post created ...'
+//     });
+// });
+
+// Advanced version - with authorization check
+app.post('/api/posts', verifyToken, (req, res)=> {
+
+    jwt.verify(req.token, SECRET_KEY, (err, authData) => {
+        if (err) {
+            res.sendStatus(403);
+        } else {
+            res.json({
+                message: 'Post created ...',
+                authData
+            });
+        }
+    })
 });
 
 app.post('/api/login', (req, res) => {
@@ -27,10 +45,33 @@ app.post('/api/login', (req, res) => {
         email: 'brad@gmail.com'
     }
 
-    jwt.sign({user: user}, 'secretkey', (err, token) => {
+    jwt.sign({user: user}, SECRET_KEY, { expiresIn: '30s'}, (err, token) => {
         res.json({
             token: token
         });
     });
 
 });
+
+// FORMAT OF TOKEN
+// Authorization: Bearer <access_token>
+
+// Verify Token
+function verifyToken(req, res, next) {
+    //Get auth header value
+    const bearerHeader = req.headers['authorization'];
+    // Check if bearer is undefined
+    if (typeof bearerHeader !== 'undefined') {
+        // Split at the space
+        const bearer = bearerHeader.split(' ');
+        // Get token from array
+        const bearerToken = bearer[1];
+        // Set the token inside the Request object
+        req.token = bearerToken;
+        // Next middleware
+        next();
+    } else {
+        // Forbidden  
+        res.sendStatus(403);
+    }
+}
